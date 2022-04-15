c29cfbe14b02531d314d33b5a047acd3cfa01b70
dbischoff777 <dbischoff777@gmail.com>
2022-04-16 00:59:35 +0200

    First commit
    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Visualizer from './PathfindingVisualizer/PathfindingVisualizer';
+
+function App() {
+  return (
+    <div className="App">
+    <Visualizer></Visualizer>
+    </div>
+  );
+}
+
+export default App;
